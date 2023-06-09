770ab9b9252da2e3069736e7bd8d9f1641d3f8c8
weixiansen574 <1695339869@qq.com>
2023-06-09 14:07:33 +0800

    v2.0.0

--- SettingsDontThroughTheList/app/src/main/java/top/weixiansen574/settingsdontthroughthelist/HookSettings.java
+++ SettingsDontThroughTheList/app/src/main/java/top/weixiansen574/settingsdontthroughthelist/HookSettings.java
@@ -22,7 +22,6 @@ public class HookSettings implements IXposedHookLoadPackage {
                 @Override
                 protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                     super.beforeHookedMethod(param);
-                    XposedBridge.log("onCreate已执行");
                 }
                 @Override
                 protected void afterHookedMethod(MethodHookParam param) throws Throwable {
@@ -32,13 +31,32 @@ public class HookSettings implements IXposedHookLoadPackage {
                     Method getIntentMethod = thisObject.getClass().getMethod("getIntent");
                     Intent intent = (Intent) getIntentMethod.invoke(thisObject);
                     XposedBridge.log("settingsIntent:" + intent.toString());
+                    if (intent.getData() != null) {
+                        XposedBridge.log("settingsIntentData:" + intent.getData().toString());
+                    }
+                    if (intent.getExtras() != null){
+                        XposedBridge.log("settingsIntentExtras:" + intent.getExtras().toString());
+                    }
                     if (intent.getAction().equals("android.settings.action.MANAGE_OVERLAY_PERMISSION")){
                         //intent中的data Uri 示例： package:com.xxx.xxxxxxx ，故去掉前面的package就是应用包名
                         String packageName = intent.getData().toString().substring(8);
                         Intent intentOpenSub = new Intent(context,loadPackageParam.classLoader.loadClass("com.android.settings.SubSettings"));
                         intentOpenSub.setAction("android.intent.action.MAIN");
+                        intentOpenSub.putExtra(":settings:show_fragment_title",packageName);
                         intentOpenSub.putExtra(":settings:source_metrics",221);
                         intentOpenSub.putExtra(":settings:show_fragment","com.android.settings.applications.appinfo.DrawOverlayDetails");
+                        Bundle bundle1 = new Bundle();
+                        bundle1.putString("package",packageName);
+                        intentOpenSub.putExtra(":settings:show_fragment_args",bundle1);
+                        context.startActivity(intentOpenSub);
+                    } else if (intent.getAction().equals("android.settings.MANAGE_UNKNOWN_APP_SOURCES")){
+                        //intent中的data Uri 示例： package:com.xxx.xxxxxxx ，故去掉前面的package就是应用包名
+                        String packageName = intent.getData().toString().substring(8);
+                        Intent intentOpenSub = new Intent(context,loadPackageParam.classLoader.loadClass("com.android.settings.SubSettings"));
+                        intentOpenSub.setAction("android.intent.action.MAIN");
+                        intentOpenSub.putExtra(":settings:show_fragment_title",packageName);
+                        intentOpenSub.putExtra(":settings:source_metrics",808);
+                        intentOpenSub.putExtra(":settings:show_fragment","com.android.settings.applications.appinfo.ExternalSourcesDetails");
                         Bundle bundle1 = new Bundle();
                         bundle1.putString("package",packageName);
                         intentOpenSub.putExtra(":settings:show_fragment_args",bundle1);
